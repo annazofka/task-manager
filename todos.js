e1146fd8a257f022daaa5fd8304a3b90d9d57524
Kocia <annazofka@gmail.com>
2023-09-09 21:56:15 +0100

    add media queries

--- todos.js
+++ todos.js
@@ -7,33 +7,21 @@ const list = document.querySelector('#task-list')
 
 let selectedPriority = 'High'
 
-// const tasks = [
-// 	{ name: 'pouczyć się JSa', priority: 'high' },
-// 	{ name: 'pouczyć się HTMLa', priority: 'low' },
-// ]
-
 const generateTemplate = (task, priority) => {
-	// 	const html = `
-	//     <li class="table-row__task-list">
-	//     <p class="table-row__task-list__task-name">${task}</p>
-	//     <p class="table-row__task-list__priority">${priority}</p>
-	// 	   <p class="table-row__task-list__done"></p>
-	//     <i class="fa-solid fa-trash delete" style="color: #1d1d1b"></i>
-
-	// </li>
-	//     `
-
 	const html = `
     <p class="table-row__task-list__task-name">${task}</p>
     <p class="table-row__task-list__priority">${priority}</p>
-	<input type="checkbox" class="table-row__task-list__done">
-	<p class="table-row__task-list__done"></p>
-    <i class="fa-solid fa-trash delete" style="color: #1d1d1b"></i>
+	<p class="table-row__task-list__done">
+		<input type="checkbox">
+	</p>
+	<p class="table-row__task-list__delete">
+		<i class="fa-solid fa-trash delete" style="color: #1d1d1b">
+		</i>
+	</p>
 `
 
 	const liNode = document.createElement('li')
 	liNode.classList.add('table-row__task-list')
-	// list.innerHTML += html
 
 	liNode.innerHTML = html
 	list.appendChild(liNode)
@@ -51,38 +39,29 @@ addNewTask.addEventListener('submit', event => {
 		highPriorityButton.classList?.remove('priority__high__red')
 		mediumPriorityButton?.classList.remove('priority__medium__yellow')
 		lowPriorityButton?.classList.remove('priority__low__green')
-		choosePriority.reset()
 	}
 })
 
 // select priority
-choosePriority.addEventListener(
-	'click',
-	event => {
-		if (event.target.id === 'high') {
-			selectedPriority = 'High'
-			highPriorityButton.classList.add('priority__high__red')
-		} else if (event.target.id === 'medium') {
-			selectedPriority = 'Medium'
-			mediumPriorityButton.classList.add('priority__medium__yellow')
-		} else if (event.target.id === 'low') {
-			selectedPriority = 'Low'
-			lowPriorityButton.classList.add('priority__low__green')
-		}
-
-		console.log(event.target.id)
-		// for (const priority of choosePriority) {
-		// 	if (priority.matches('.choose-priority__high')) {
-		// 		console.log('priority should be high but how?')
+choosePriority.addEventListener('click', event => {
+	if (event.target.id === 'high') {
+		selectedPriority = 'High'
+		highPriorityButton.classList.add('priority__high__red')
+	} else if (event.target.id === 'medium') {
+		selectedPriority = 'Medium'
+		mediumPriorityButton.classList.add('priority__medium__yellow')
+	} else if (event.target.id === 'low') {
+		selectedPriority = 'Low'
+		lowPriorityButton.classList.add('priority__low__green')
 	}
-
-	// const priority = choosePriority.event.target
-	// alert('button clicked')
-)
+})
 
 // delete tasks
 list.addEventListener('click', event => {
 	if (event.target.classList.contains('delete')) {
-		event.target.parentElement.remove()
+		event.target.parentElement.parentElement.remove()
 	}
 })
+
+// pagination
+// to be added later
